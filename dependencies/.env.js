1fd718eaa7b5ccbbef56d0782232c5acaca86ce9
Zachary Rener <me@zacharyrener.com>
2020-09-14 21:25:19 -0500

    moved files to dependencies folder

---
+++ dependencies/.env.js
@@ -0,0 +1,2 @@
+const SITE_URL = "testing.local";
+exports.SITE_URL = SITE_URL;
---
+++ dependencies/webpack.config.js
@@ -0,0 +1,72 @@
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const config = require("./.env.js");
+
+module.exports = {
+  mode: "development",
+  entry: "./scripts/main.ts",
+  output: {
+    path: __dirname + "/build",
+    filename: "main.js",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["react", "es2015"] },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new BrowserSyncPlugin(
+      // BrowserSync options
+      {
+        // browse to http://localhost:3000/ during development
+        host: "localhost",
+        port: 3000,
+        // proxy the Webpack Dev Server endpoint
+        // (which should be serving on http://localhost:3100/)
+        // through BrowserSync
+        proxy: config.SITE_URL,
+      },
+      // plugin options
+      {
+        // prevent BrowserSync from reloading the page
+        // and let Webpack Dev Server take care of this
+        reload: true,
+      }
+    ),
+  ],
+};
