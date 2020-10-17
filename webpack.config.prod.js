4fd8b8117896571ecd74810668c45c4dd918975d
Zachary Rener <me@zacharyrener.com>
2020-10-17 14:47:02 -0500

    removed all files for fresh start

--- webpack.config.prod.js
+++
@@ -1,7 +0,0 @@
-const imagemin = require("imagemin");
-const webp = require("imagemin-webp");
-
-imagemin(["images/*.{jpg,png}"], {
-  destination: "build/images",
-  plugins: [webp({ quality: 60 })],
-});
